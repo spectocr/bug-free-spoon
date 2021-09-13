5af2fd7a2093f396250e3331d6d474320a9b4274
spectocr <highstakes22@gmail.com>
2021-09-13 19:49:14 -0400

    push the bones

---
+++ client/build/precache-manifest.1c357f14bb3df449cbd9842c808e7012.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108c98cbe645c952f0c8fd34f4be552a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfc14bad2c5f44f97334",
+    "url": "/static/css/main.ad1af64d.chunk.css"
+  },
+  {
+    "revision": "ebfe4975b4191a362b4a",
+    "url": "/static/js/2.d3d2ec8f.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.d3d2ec8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfc14bad2c5f44f97334",
+    "url": "/static/js/main.2c24dd6e.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
