56cb706b210a505cd8417a5227cbedd6ddb472b0
spectocr <highstakes22@gmail.com>
2021-09-14 16:54:52 -0400

    adding seed, resolver, and typedefs for realtor changes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,16 +11,23 @@ const typeDefs = gql`
     name: String
     description: String
     image: String
-    agency: String
+    quantity: Int
+    price: Float
+    category: Category
   }
 
-
+  type Order {
+    _id: ID
+    purchaseDate: String
+    realtors: [Realtor]
+  }
 
   type User {
     _id: ID
     firstName: String
     lastName: String
     email: String
+    orders: [Order]
   }
 
   type Auth {
@@ -28,10 +35,20 @@ const typeDefs = gql`
     user: User
   }
 
+  type Query {
+    categories: [Category]
+    realtors(category: ID, name: String): [Realtor]
+    realtor(_id: ID!): Realtor
+    user: User
+    order(_id: ID!): Order
+    checkout(realtors: [ID]!): Checkout
+  }
 
   type Mutation {
     addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addOrder(realtors: [ID]!): Order
     updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateRealtor(_id: ID!, quantity: Int!): Realtor
     login(email: String!, password: String!): Auth
   }
 
